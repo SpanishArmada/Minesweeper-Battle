e2bddce687aaee9845799098bd23b12f1c13e0f5
dbakti7 <dbakti1605@gmail.com>
2016-03-12 17:14:32 +0800

    mapGenerator.js updated

--- server/inc/mapGenerator.js
+++ server/inc/mapGenerator.js
@@ -23,12 +23,12 @@ function shuffle(array) {
     return array;
 }
 
-function getBombs(xSize, ySize, xClicked, yClicked, bombs) {
+function shuffleGrids(xSize, ySize, xClicked, yClicked, bombs) {
     /**
      * (xSize, ySize) is the board size
      * (xClicked, yClicked) is the grid clicked by user at the first time
      * bombs is the number of bombs that we want on the board
-     * return the coordinates of bombs
+     * return the shuffled-coordinates
      * @type {Array}
      */
     var grids = [];
@@ -41,5 +41,81 @@ function getBombs(xSize, ySize, xClicked, yClicked, bombs) {
         }
     }
     shuffle(grids);
-    return grids.slice(0, bombs);
+    return grids;
+}
+
+function placeBombs(xSize, ySize, shuffledGrids, bombs) {
+    /**
+     * (xSize, ySize) is the board size
+     * bombs is the number of bombs that we want on the board
+     * return grids: 2D array, 0 for bomb, 1 - 8 for bombs count
+     * @type {Array}
+     */
+    var grids = new Array(xSize);
+    for(i=0;i<xSize;++i)
+        grids[i] = new Array(ySize);
+
+    for(i=0;i<xSize;++i)
+        for(j=0;j<ySize;++j)
+            grids[i][j] = 0;
+
+    var bombLocations = new Array(xSize);
+    for(i=0;i<xSize;++i)
+        bombLocations[i] = new Array(ySize);
+
+    for(i=0;i<xSize;++i)
+        for(j=0;j<ySize;++j)
+            bombLocations[i][j] = 0;
+
+    for(coordinate in shuffledGrids) {
+        if(bombs == 0)
+            break;
+
+        ++grids[coordinate[0]+1][coordinate[1]];
+        ++grids[coordinate[0]-1][coordinate[1]];
+        ++grids[coordinate[0]][coordinate[1]+1];
+        ++grids[coordinate[0]][coordinate[1]-1];
+        ++grids[coordinate[0]+1][coordinate[1]+1];
+        ++grids[coordinate[0]-1][coordinate[1]-1];
+        ++grids[coordinate[0]-1][coordinate[1]+1];
+        ++grids[coordinate[0]+1][coordinate[1]-1];
+
+        if((++grids[coordinate[0]+1][coordinate[1]] == 9) ||
+            (++grids[coordinate[0]-1][coordinate[1]] == 9) ||
+            (++grids[coordinate[0]][coordinate[1]+1] == 9) ||
+            (++grids[coordinate[0]][coordinate[1]-1] == 9) ||
+            (++grids[coordinate[0]+1][coordinate[1]+1] == 9) ||
+            (++grids[coordinate[0]-1][coordinate[1]-1] == 9) ||
+            (++grids[coordinate[0]-1][coordinate[1]+1] == 9) ||
+            (++grids[coordinate[0]+1][coordinate[1]-1] == 9)) {
+
+            --grids[coordinate[0]+1][coordinate[1]];
+            --grids[coordinate[0]-1][coordinate[1]];
+            --grids[coordinate[0]][coordinate[1]+1];
+            --grids[coordinate[0]][coordinate[1]-1];
+            --grids[coordinate[0]+1][coordinate[1]+1];
+            --grids[coordinate[0]-1][coordinate[1]-1];
+            --grids[coordinate[0]-1][coordinate[1]+1];
+            --grids[coordinate[0]+1][coordinate[1]-1];
+        }
+        else {
+            --bombs;
+            bombLocations[coordinate[0]][coordinate[1]] = 1;
+        }
+    }
+    for(i=0;i<xSize;++i) {
+        for(j=0;j<ySize;++j) {
+            if(bombLocations[i][j] == 1)
+                grids[i][j] = 0;
+        }
+    }
+    return grids;
+}
+
+
+function getMap(xSize, ySize, xClicked, yClicked, bombs) {
+
+    shuffledGrids = shuffleGrids(xSize, ySize, xClicked, yClicked, bombs);
+    grids = placeBombs(xSize, ySize, shuffledGrids, bombs);
+    return grids;
 }
