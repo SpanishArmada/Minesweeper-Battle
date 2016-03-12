15c84b2a3ac5ba41046f3300dafba720f144981d
cacad101 <gbbpentium@gmail.com>
2016-03-12 22:57:10 +0800

    Updates

--- server/inc/GameConstant.js
+++ server/inc/GameConstant.js
@@ -2,6 +2,9 @@ module.exports = (function () {
 	return {
 		NUM_ROWS: 16,
 		NUM_COLS: 16,
-		NUM_MINES: 41
+		NUM_MINES: 41,
+		CORRECT_FLAG: 1,
+		WRONG_FLAG: -1,
+		MINES_EXPLODES: -5
 	}
 }());
