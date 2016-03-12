011e3e550c068e7d42a41c9c772502a16968168a
Peter <peterch1ang@yahoo.com>
2016-03-12 21:00:34 +0800

    Added example for Websocket client

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 // All requires
 var
 	config = require('./inc/Config.js'),
-	ws = require('ws'),
+	WebSocket = require('ws'),
 
 	MessageType = require('./inc/MessageType.js'),
 	BoardGenerator = require('./inc/BoardGenerator.js'),
 	GameConstant = require('./inc/GameConstant.js'),
 	MatchmakingQueue = require('./inc/MatchmakingQueue.js');
 
+var wss = new WebSocket.Server({ port: config.port() });
+
+wss.on('connection', function (ws) {
+	console.log('Number of clients: %d', wss.clients.length);
+
+	ws.on('message', function (msgStr) {
+		var data = JSON.parse(msgStr);
+	})
+
+	ws.on('close', function () {
+		console.log('Number of clients: %d', wss.clients.length);
+	})
+});
