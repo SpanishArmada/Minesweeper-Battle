ee760e8dce1e5cffdd740b716d2a5a2c7b477947
Peter <peterch1ang@yahoo.com>
2016-03-12 15:32:23 +0800

    Added config file for setting up a server

---
+++ server/inc/config.js
@@ -0,0 +1,38 @@
+module.exports = (function () {
+	var port = 80,
+		protocol = 'ws',
+		hostname = 'localhost';
+
+	return {
+		port: function (arg) {
+			if(typeof arg === 'undefined') {
+				return port;
+			}
+
+			port = parseInt(arg, 10) || port;
+			return port;
+		},
+
+		protocol: function (arg) {
+			if(typeof arg === 'undefined') {
+				return protocol;
+			}
+
+			protocol = ('' + arg) || protocol;
+			return protocol;
+		},
+
+		hostname: function (arg) {
+			if(typeof arg === 'undefined') {
+				return hostname;
+			}
+
+			hostname = ('' + arg) || hostname;
+			return hostname;
+		},
+
+		getFullUrl: function () {
+			return protocol + '://' + hostname + ':' + port + '/';
+		}
+	}
+}());
