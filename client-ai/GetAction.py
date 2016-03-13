94d64ccaca64183ecb12278c3bec5d02a77fc335
dbakti7 <dbakti1605@gmail.com>
2016-03-13 08:37:52 +0800

    AI updated

--- client-ai/GetAction.py
+++ client-ai/GetAction.py
@@ -8,32 +8,32 @@ def getAction(numRows, numCols, gameState):
     for i in range(numRows):
         for j in range(numCols):
             curGrid = gameState[i][j]
-            if(curGrid == -1 or (curGrid >= 9)):
+            if(curGrid == -1 or curGrid >= 9):
                 continue
             walls = 0
             revealedBombs = 0
             for k in range(len(dr)):
-                if(inBoard(numRows, numCols, i + dr, j + dc)):
-                    if(0 <= gameState[i + dr][j + dc] <= 8):
-                        ++walls
-                    elif(gameState[i + dr][j + dc] >= 9):
-                        ++revealedBombs
+                if(inBoard(numRows, numCols, i + dr[k], j + dc[k])):
+                    if(0 <= gameState[i + dr[k]][j + dc[k]] <= 8):
+                        walls += 1
+                    elif(gameState[i + dr[k]][j + dc[k]] >= 9):
+                        revealedBombs += 1
                 else:
-                    ++walls
-
+                    walls += 1
+            
             space = 8 - walls - revealedBombs
             curGrid -= revealedBombs
+            print(i, j, walls, revealedBombs, space, curGrid)
             if(curGrid == 0):
                 # openGrid
                 for k in range(len(dr)):
-                    if(inBoard(numRows, numCols, i + dr, j + dc)):
-                        if(gameState[i + dr][j + dc] == -1):
-                            return ['open', i + dr, j + dc]
+                    if(inBoard(numRows, numCols, i + dr[k], j + dc[k])):
+                        if(gameState[i + dr[k]][j + dc[k]] == -1):
+                            return ['open', i + dr[k], j + dc[k]]
             elif(curGrid == space):
                 # flagGrid
                 for k in range(len(dr)):
-                    if(inBoard(numRows, numCols, i + dr, j + dc)):
-                        if(gameState[i + dr][j + dc] == -1):
-                            return ['flag', i + dr, j + dc]
-            else:
-                return ['finish']
+                    if(inBoard(numRows, numCols, i + dr[k], j + dc[k])):
+                        if(gameState[i + dr[k]][j + dc[k]] == -1):
+                            return ['flag', i + dr[k], j + dc[k]]
+    return ['finish']
--- client-ai/main.py
+++ client-ai/main.py
@@ -2,29 +2,36 @@ import websocket
 import threading
 import time
 import json
+import GetAction
+from GetAction import getAction
 
-getActionRunning = False
+getActionRunning = None
 def on_message(ws, message):
     # edit here for getAction
+    global getActionRunning
     print(message)
     obj = json.loads(message)
-    if(obj[0] == 'gameState'):
+    if(obj['type'] == 'gameState'):
         content = obj['content']
         board = content['board']
         numRows = len(board)
         numCols = len(board[0])
+        print(numRows, numCols)
         if(not getActionRunning):
             getActionRunning = True
             while(True):
                 action = getAction(numRows, numCols, board)
+                print(action)
                 if(action[0] == "open"):
-                    sendMessage = ["clickReveal", {i: action[1], j: action[2]}]
+                    sendMessage = {"type": "clickReveal", "content": {"i": action[1], "j": action[2]}}
                     ws.send(json.dumps(sendMessage))
                     time.sleep(0.5)
+                    break
                 elif(action[0] == "flag"):
-                    sendMessage = ["clickFlag", {i: action[1], j: action[2]}]
+                    sendMessage = {"type": "clickFlag", "content": {"i": action[1], "j": action[2]}}
                     ws.send(json.dumps(sendMessage))
                     time.sleep(0.5)
+                    break
                 elif(action[0] == "finish"):
                     break
                 
@@ -38,22 +45,29 @@ def on_error(ws, error):
     print(error)
 
 def on_close(ws):
+    ws.close()
     print("### closed ###")
 
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        ws.close()
-        print("thread terminating...")
-    threading.Thread(target=run).start()
+##    ws.send("s")
+    global getActionRunning
+    getActionRunning = False
+    x = json.dumps({"type":"findMatch", "content":{"name":"AI"}})
+    ws.send(x)
+##    def run(*args):
+##        x = json.dumps(["findMatch", {"name": "AI"}])
+##        ws.send(x)
+##        print(x)
+####        for i in range(3):
+####            ws.send("Hello %d" % i)
+####        #ws.close()
+####        print("thread terminating...")
+##    threading.Thread(target=run).start()
 
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
+    ws = websocket.WebSocketApp("ws://spanisharmada-pciang.rhcloud.com:8000/",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
